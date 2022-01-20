f9a8de0d8ed4a1b82bea02a02d08da9013cd84d5
Garrett Boling <gboling829@gmail.com>
2022-01-20 08:49:58 -0500

    started on lab 3

---
+++ labs/lab3/lab3a.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <math.h>
+int main(void)
+{
+    // initializing variables //
+
+    const int noSides = 5, radius = 5;
+    float angleRad, angleDeg, sideLen, periIn, periFt, partialResults;
+}
---
+++ labs/lab3/lab3b.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <math.h>
+int main(void)
+{
+    // initializing variables //
+
+    int noSides, radius;
+    float angleRad, angleDeg, sideLen, periIn, periFt, partialResults;
+
+    printf("Number of sides: ");
+    scanf("%i", &noSides);
+
+    printf("Radius: ");
+    scanf("%i", &radius);
+}
