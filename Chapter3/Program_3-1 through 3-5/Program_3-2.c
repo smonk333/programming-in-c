f0351da9dda3a2517c5b856e683872a1e4af2410
smonk333 <gboling829@gmail.com>
2022-01-25 18:13:21 -0500

    added chapter5

---
+++ Chapter3/Program_3-1 through 3-5/Program_3-2.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+int main(void)
+{
+	 /////////////////////////
+	// Declaring Variables //
+   /////////////////////////
+	int a = 100, b = 2, c = 25, d = 4, result;
+
+	result = a - b; 	//subtraction
+	printf ("a - b = %i\n", result);
+
+	result = b * c; 	//multiplication
+	printf ("b * c = %i\n", result);
+
+	result = a / c; 	//division
+	printf ("a / c = %i\n", result);
+
+	result = a + b * c; 	//precedence
+	printf ("a + b * c = %i\n", result);
+
+	printf ("a * b + c * d = %i\n", a * b + c * d);
+
+	return 0;
+}
