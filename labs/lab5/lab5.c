ee91f4136928a64c19370eaef292633e655e474d
smonk333 <gboling829@gmail.com>
2022-01-28 19:52:38 -0500

    did more work

--- labs/lab5/lab5.c
+++ labs/lab5/lab5.c
@@ -1,47 +1,50 @@
 #include<stdio.h>
 int main(void)
 {
-	int input, intOutput, yesNo, evenOdd, inputType;
-	char charOutput;
+	int input, test, yesNo, inputType;
+	char charInput, charOutput;
 	/* inputType is used to determine how to print the output.
-	   setting it to 1 means the output will be printed using %i,
-	   setting it to 0 means the output will be printed using %c.
+	   setting it to 1 means the output will be printed as "even" or "odd",
+	   setting it to 2 or 3 means the output will be printed using %c.
 
 	   yesNo is used to gather the input from $user to determine if the 
 	   while loop should run again.
-
-	   evenOdd is used to hold info about whether input / 2 gives a remainder.
-	   odd = 1
-	   even = 0
 	*/
 
 	yesNo = 1;
 
 	while(yesNo = 1) {
 		printf("\nEnter a letter or number from the keyboard: ");
-		scanf("%c", &input);
-
-		if(input >= 0 && input <= 9) {
-			if(input % 2 == 1) {
-				inputType = 1;	// input was an even number
-				evenOdd = 1;
-			}
-			else {
-				inputType = 1;  // input was an odd number
-				evenOdd = 0;
-			}
-		}
-		else if(input >= 65 && input <= 90) {
-			inputType = 0;			// input was an uppercase letter
-			charOutput = input + 32;
-		}
-		else if(input >= 97 && input <= 122) {
-			inputType = 0;			//input was a lowercase letter
-			charOutput = input - 32;
-		}
+		scanf("%i", &input);
+		charInput = (char) input;
+		if(charInput >= 48 && charInput <= 57)
+			inputType = 1; // input was a number
+		else if(charInput >= 65 && charInput <= 90)
+			inputType = 2; // input was an uppercase letter
+		else if(charInput >= 97 && charInput <= 122)
+			inputType = 3; // input was a lowercase letter
 		else {
 			printf("\ninvalid character entered.\n");
 		}
+
+		if(inputType == 0) {
+			test = input % 2;
+			if(test == 1) {
+				printf("You entered an odd number\n");
+			}
+			else {
+				printf("You entered an even number\n");
+			}
+		}
+		else if(inputType == 2) {
+			charOutput = charInput + 32;
+			printf("The lower case of that letter is %c", charOutput);
+		}
+		else if(inputType == 3) {
+			charOutput = charInput - 32;
+			printf("The upper case of that letter is %c", charOutput);
+		}
+
 
 		printf("Would you like to go again? Enter 1 for yes, 0 for no. : ");
 		scanf("%i", &yesNo);
