99c198ef4298bca34a88a038de94447ce8d171f9
smonk333 <gboling829@gmail.com>
2022-02-01 18:53:24 -0500

    added 6.2

---
+++ chapter6/programsCh6/program_6-2.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+int main(void)
+{
+	int ratingCounters[11], i, response;
+
+	for(i = 1; i <= 10; ++i)
+		ratingCounters[i] = 0;
+
+	printf("Enter your responses\n");
+
+	for(i = 1; i <= 20; ++i) {
+		scanf("%i", &response);
+		if(response < 1 || response > 10)
+			printf("Bad response: %i\n", response);
+		else
+			++ratingCounters[response];
+	}
+
+	printf("\n\nRating  Number of responses\n");
+	printf("------ --------------------\n");
+
+	for(i = 1; i <= 10; ++i)
+		printf("%4i%14i\n", i, ratingCounters[i]);
+
+	return 0;
+}
