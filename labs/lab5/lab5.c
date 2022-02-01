d4d3df581afd1e777b65668329ba93b20d8074bc
garrett boling <gboling829@gmail.com>
2022-02-01 11:04:14 -0500

    finally fixed lab5.c

--- labs/lab5/lab5.c
+++ labs/lab5/lab5.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
 int main(void)
 {
-	int input, test, yesNo = 0, inputType;
+	int  test, yesNo = 1, inputType;
 	char charInput, charOutput;
 	/* inputType is used to determine how to print the output.
 	   setting it to 1 means the output will be printed as "even" or "odd",
@@ -12,19 +12,22 @@ int main(void)
 	*/
 
 	do {
-		printf("\nEnter a letter or number from the keyboard: ");
-		scanf("%c", &charInput);
-		if(charInput >= 48 && charInput <= 57)
+		printf("Enter a letter or number from the keyboard: ");
+		scanf(" %c", &charInput);
+		if(charInput >= 48 && charInput <= 57){
 			inputType = 1; // input was a number
-		else if(charInput >= 65 && charInput <= 90)
+		}
+		else if(charInput >= 65 && charInput <= 90){
 			inputType = 2; // input was an uppercase letter
-		else if(charInput >= 97 && charInput <= 122)
+		}
+		else if(charInput >= 97 && charInput <= 122){
 			inputType = 3; // input was a lowercase letter
+		}
 		else {
 			printf("\ninvalid character entered.\n");
 		}
 
-		if(inputType == 0) {
+		if(inputType == 1) {
 			test = (int) charInput % 2;
 			if(test == 1) {
 				printf("You entered an odd number\n");
@@ -45,16 +48,15 @@ int main(void)
 
 		printf("Would you like to go again? Enter 1 for yes, 0 for no. : ");
 		scanf("%i", &yesNo);
-/*
+
 		if(yesNo != 1 && yesNo != 0){
 			while(yesNo != 1 && yesNo != 0) {
 				printf("\nInvalid input. Enter 1 to go again, or 0 to quit: ");
 				scanf("%i", &yesNo);
 			}
-		
 		}
-
-		*/
 	}
-	while(yesNo != 0);
+	while(yesNo == 1);
+	
+	return 0;
 }
