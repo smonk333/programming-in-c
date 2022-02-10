000dc954a65f355e735d379e7f6a188fa1a26a9b
garrett boling <gboling829@gmail.com>
2022-02-10 15:18:42 -0500

    started 8-8

---
+++ chapter6/programsCh6/program_6-8.c
@@ -0,0 +1,11 @@
+// generate fibonacci numbers using variable length arrays
+
+#include<stdio.h>
+
+int main(void)
+{
+	int i, numFibs;
+
+	printf("How many Fibonacci numbers do you want (between 1 and 75)?: ");
+	
+}
