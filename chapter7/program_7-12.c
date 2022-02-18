667b98899458883eb136d7ebef15142dfa6131a4
smonk333 <gboling829@gmail.com>
2022-02-17 19:44:08 -0500

    typed program_7-1 - 7-12

---
+++ chapter7/program_7-12.c
@@ -0,0 +1,38 @@
+#include<stdio.h>
+
+void sort (int a[], int n)
+{
+	int i, j, temp;
+
+	for(i = 0; i < n - 1; ++i)
+		for(j = i + 1; j < n; ++j)
+			if(a[i] > a[j]){
+				temp = a[i];
+				a[i] = a[j];
+				a[j] = temp;
+			}
+}
+
+int main(void)
+{
+	int i;
+	int array[16] = {34, -5, 6, 0, 12, 100, 56, 22, 44, -3, -9, 12, 17, 22, 6, 11};
+
+	void sort(int a[], int n);
+
+	printf("the array before the sort:\n");
+
+	for(i = 0; i < 16; ++i)
+		printf("%i ", array[i]);
+
+	sort(array, 16);
+
+	printf("\n\nthe array after the sort:\n");
+
+	for(i = 0; i < 16; ++i)
+		printf("%i ", array[i]);
+
+	printf("\n");
+
+	return 0;
+}
