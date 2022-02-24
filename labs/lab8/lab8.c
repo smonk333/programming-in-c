a4f274c3e5e21ed67a75802ee70a12a9417cb0f8
garrett boling <gboling829@gmail.com>
2022-02-24 17:27:28 -0500

    finished lab8.c

--- labs/lab8/lab8.c
+++ labs/lab8/lab8.c
@@ -1,68 +1,112 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+void initWithInput(int arr1[]);
+void initWithRandom(int arr1[]);
+int findSecondHighest(int arr1[]);
+int findHighest(int arr1[]);
+int findInnerProduct(int arr1[], int arr2[]);
+
+#define ASIZE 20
+
+void initWithInput(int arr1[])
+{
+	int i = 0, j = 0, a = 0;
+	
+	for(i = 0; i < ASIZE; ++i){
+		scanf("%i", &arr1[i]);
+	}
+	for (i = 0; i < ASIZE; ++i){
+		for (j = i + 1; j < ASIZE; ++j){
+			if (arr1[i] > arr1[j]){
+				a = arr1[i];
+				arr1[i] = arr1[j];
+				arr1[j] = a;
+			}
+		}
+	}
+	return;
+}
+
+void initWithRandom(int arr1[])
+{
+	int i = 0, j = 0, a = 0;
+
+	for (i = 0; i < ASIZE; ++i){
+		arr1[i] = rand() % 50 + 1;
+	}
+	for (i = 0; i < ASIZE; ++i){
+		for (j = i + 1; j < ASIZE; ++j){
+			if (arr1[i] > arr1[j]){
+				a = arr1[i];
+				arr1[i] = arr1[j];
+				arr1[j] = a;
+			}
+		}
+	}
+
+	return;
+}
+
+int findHighest(int arr1[])
+{
+	int max = 0, i = 0;
+	for (i = 0; i < ASIZE; ++i){
+		if(arr1[i] > max) {
+            max = arr1[i];
+        }
+  }
+  return max;      
+}
+
+int findSecondHighest(int arr1[])
+{
+	int i = 0, secMax = 0, max = findHighest(arr1);
+	for (i = 0; i < ASIZE; ++i){
+		if(secMax < max){
+			secMax = arr1[i-1];
+		}
+	}
+	return secMax;
+}
+
+int findInnerProduct(int arr1[], int arr2[])
+{
+	int i = 0, product = 0; 
+		for (i = 0; i < ASIZE; ++i){
+    	product += arr1[i] * arr2[i];
+	}
+	return product;
+}
+
 int main(void)
 {
-	int array1[20], array2[20], i, j, max1 = 0, max2 = 0, secondMax1 = 0, secondMax2 = 0, product = 0;
+	int arr1[ASIZE], arr2[ASIZE], i = 0, max1 = 0, max2 = 0, secondMax = 0, product = 0;
 
-	// initialize array1
+	printf("Enter 20 integers to initialize arr1: ");
+	initWithInput(arr1);
 
-	printf("Enter 20 integers to initialize array1: ");
+	printf("\n\narr1:\n");
 
-	for (i = 0; i < 20; ++i){
-		scanf("%i", &array1[i]);
+	for(i = 0; i < ASIZE; ++i){
+		printf("%i\n", arr1[i]);
+	}
+		
+	printf("\n\nInitializing arr2...\n\narr2:\n");
+	initWithRandom(arr2);
+
+	for(i = 0; i < ASIZE; ++i){
+		printf("%i\n", arr2[i]);
 	}
 
-	printf("\narray1:\n");
+	secondMax = findSecondHighest(arr1);
+	printf("\nSecond highest in arr1 is: %i\n", secondMax);
 
-	for (i = 0; i< 20; ++i){
-		printf("%i\n", array1[i]);
-	}
+	secondMax = findSecondHighest(arr2);
+	printf("\nSecond highest in arr1 is: %i\n", secondMax);
 
-	// initialize array2
-
-	for (i = 0; i < 20; ++i){
-		array2[i] = rand() % 50 + 1;
-	}
-
-	printf("\narray2:\n");
-
-	for (i = 0; i < 20; ++i){
-		printf("%i\n", array2[i]);
-	}
-
-	for (i = 0, j = 0; i < 20 && j < 20; ++i, ++j){
-
-		if(array1[i] > max1) {
-            secondMax1 = max1;
-            max1 = array1[i];
-        }
-
-        else{
-			if(array1[i] > secondMax1){
-				secondMax1 = array1[i];
-			}
-	    }
-
-	    if(array2[j] > max2) {
-            secondMax2 = max2;
-            max2 = array2[j];
-        }
-
-        else{
-			if(array2[j] > secondMax2){
-				secondMax2 = array2[j];
-			}
-	    }
-
-	}
-
-	for (i = 0; i < 20; ++i){
-    	product += array1[i] * array2[i];
-	}
-
-	printf("\nsecond highest from array1: %i\nsecond highest from array2: %i\n", secondMax1, secondMax2);
-	printf("\ninner product is: %i", product);
+	product = findInnerProduct(arr1, arr2);
+	printf("\n\ninner product is: %i\n", product);
 
 	return 0;
 }
