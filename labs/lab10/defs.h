73c581d65dc6a0ca486fc44094f508334b753472
smonk333 <gboling829@gmail.com>
2022-03-26 15:06:31 -0400

    chapter 10

---
+++ labs/lab10/defs.h
@@ -0,0 +1,15 @@
+#ifndef defs
+#define defs
+
+typedef struct {
+	char item[20];
+	char quantity[10];
+	int calories;
+	float protein;
+	float carbs;
+	float fats;
+} food;
+void printArray(int firstLine, food foodIn[8]);
+
+
+#endif
